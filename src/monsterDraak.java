d73abd8d1ddf7fa4f62d95626e15299b4db292b4
tiescs <151636636+tiescs@users.noreply.github.com>
2025-05-20 10:34:32 +0200

    CLI Meerdere Soorten Kamers Toegevoegd + Observer pattern

--- src/monsterDraak.java
+++
@@ -1,5 +0,0 @@
-class monsterDraak implements Monster {
-    public void soort() {
-        System.out.println("FOUTTTTTTTTTTTTTTTTT!!!!!!!!!");
-    }
-}
--- src/monsterGorilla.java
+++
@@ -1,5 +0,0 @@
-class monsterGorilla implements Monster {
-    public void soort() {
-        System.out.println("Ooga ooga jij hebt deze vraag fout! booga booga ");
-    }
-}
