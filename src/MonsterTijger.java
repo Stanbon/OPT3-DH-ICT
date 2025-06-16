d9a61f8882bdb5c563efad7544a6f134ffc3af2c
SweetAmaliaXO <86151222+SweetAmaliaXO@users.noreply.github.com>
2025-06-16 12:38:24 +0200

    Duplicate code smells uit Monsters gehaald

--- src/MonsterTijger.java
+++ src/MonsterTijger.java
@@ -1,55 +1,5 @@
-public class MonsterTijger implements Monster, AntwoordObserver {
-
-    private int hp = 60;
-    private final String naam = "Tijger";
-    private boolean verslagen = false;
-    private final int schadeWaarde = 8;
-
-    @Override
-    public void update(boolean correctAntwoord) {
-        if (!correctAntwoord) {
-            verschijn();
-        } else {
-            verdwijn();
-        }
+public class MonsterTijger extends BaseMonster {
+    public MonsterTijger() {
+        super("Tijger", 60, 8);
     }
-
-    @Override
-    public void verschijn() {
-        System.out.println(naam + " verschijnt in de kamer!");
-    }
-
-    @Override
-    public void verdwijn() {
-        System.out.println(naam + " verdwijnt uit de kamer!");
-    }
-
-    @Override
-    public int getHP() { return hp;}
-
-    @Override
-    public String getNaam() { return naam; }
-
-    @Override
-    public void ontvangSchade(int schade) {
-        hp -= schade;
-        if (hp < 0) hp = 0;
-        System.out.println(naam + " ontvangt " + schade + " schade! HP over: " + hp);
-        if (hp == 0) {
-            verslagen = true;
-            System.out.println(naam + " is verslagen!");
-        }
-    }
-
-    @Override
-    public void valAan(Vechten doel) {
-        System.out.println(naam + " valt " + doel.getNaam() + " aan en doet " + schadeWaarde + " schade!");
-        doel.ontvangSchade(schadeWaarde);
-    }
-
-    @Override
-    public boolean isVerslagen() {
-        return verslagen;
-    }
-
 }
