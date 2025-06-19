481785e85b15929b4e8e36363cb1b27c778c6ac9
SweetAmaliaXO <86151222+SweetAmaliaXO@users.noreply.github.com>
2025-06-19 10:28:39 +0200

    Meer hints toegevoegd

--- src/FunnyHintProvider.java
+++ src/FunnyHintProvider.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class FunnyHintProvider extends BaseHintProvider {
     public FunnyHintProvider() {
         super(
-                List.of("Heb je ooit geprobeerd links te kijken?", "Misschien helpt een dansje."),
+                List.of("Heb je ooit geprobeerd links te kijken?", "Misschien helpt een dansje.", "Of je kunt gewoon de deur uitlopen.", "Of je kunt gewoon gokken, dat werkt ook soms.","Waarom niet gewoon alles proberen?"),
                 "Wil je een hint? Ja/Nee"
         );
     }
--- src/HelpHintProvider.java
+++ src/HelpHintProvider.java
@@ -3,7 +3,7 @@ import java.util.List;
 public class HelpHintProvider extends BaseHintProvider {
     public HelpHintProvider() {
         super(
-                List.of("Denk aan de Daily Scrum.", "Wat was het doel van die artefact ook alweer?"),
+                List.of("Denk aan de Daily Scrum.", "Wat was het doel van die artefact ook alweer?", "Misschien helpt het om te denken aan de Scrum Guide.", "Probeer te focussen op de samenwerking binnen het team.", "Wat zou je teamleider zeggen?", "Misschien is het handig om te denken aan de rollen binnen Scrum.", "Wat zou de Product Owner doen in deze situatie?"),
                 "Wil je een hint? Ja/Nee"
         );
     }
